ba6f1bbe8d92046980c630dad0a0ab8403897859
justbenice-mp3 <justbenice.mp3@gmail.com>
2026-05-28 00:53:09 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { ThemeProvider } from './context/ThemeContext'
+import { PremiumProvider } from './context/PremiumContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <PremiumProvider>
+          <App />
+        </PremiumProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
